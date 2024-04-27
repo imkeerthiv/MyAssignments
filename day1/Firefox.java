435fb9d6c6b219c9ab3beb9b4260dc158f9a2d16
imkeerthiv <168268924+imkeerthiv@users.noreply.github.com>
2024-04-27 14:54:01 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion = 100.2f;
+String browserName = "firefox";
+boolean isVisible = true;
+int releaseYear = 1998;
+char browserLogo = 'f';
+System.out.println(browserVersion + "\n" +browserName + "\n" +isVisible + "\n" +releaseYear + "\n" +browserLogo);
+	}
+
+}
